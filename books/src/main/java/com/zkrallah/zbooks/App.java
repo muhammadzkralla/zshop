37c50e5e11ab7c50382adac430ea34e8a862840e
zkrallah <muhammad.heshamyt@gmail.com>
2025-07-08 15:34:50 +0300

    Separated project files.

---
+++ books/src/main/java/com/zkrallah/zbooks/App.java
@@ -0,0 +1,13 @@
+package com.zkrallah.zbooks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
